8a8d738c2db6f4368d35dc64f52bbf2199cd2a74
kelvzxu <ceokelvin12@gmail.com>
2021-01-03 22:21:17 +0700

    Fix Bugs page scroll

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -4,11 +4,17 @@
       <jet-sidebar>
         <jet-sidebar-primary>
           <div class="sidebar-navigation-items">
-            <div class="sidebar-icon-wrapper">
-              <div class="sidebar-navigation-action-component">
-                <span class="fa fa-th-large text-white"> </span>
+            <inertia-link
+              :href="route('dashboard')"
+              :active="route().current('dashboard')"
+              class="button_link"
+            >
+              <div class="sidebar-icon-wrapper">
+                <div class="sidebar-navigation-action-component">
+                  <span class="fa fa-th-large text-white"> </span>
+                </div>
               </div>
-            </div>
+            </inertia-link>
             <div class="sidebar-icon-wrapper">
               <div class="sidebar-navigation-action-component">
                 <span class="fas fa-bell text-white"> </span>
@@ -115,91 +121,8 @@
           </template>
         </jet-sidebar-secondary>
       </jet-sidebar>
-      <div class="application-content">
-        <div id="application-control">
-          <div class="application-control-component is-expanded">
-            <div class="application-control-button-component-wrap">
-              <div
-                disabled=""
-                class="application-control-button-component is-pinned"
-              >
-                <span class="collapse-icon fa fa-angle-right"></span>
-              </div>
-            </div>
-            <div class="first-level-control-content">
-              <div class="home-control-component">
-                <div class="home-workspace-items-component">
-                  <div class="content-wrapper">
-                    <div class="home-workspace-items-title-component">
-                      <div class="home-workspace-items-header">
-                        <div class="workspace-content">
-                          <div
-                            class="workspace-icon-container workspace-size-md"
-                          >
-                            <div
-                              class="workspace-icon-shape-container"
-                              style="background-color: rgb(242, 121, 242)"
-                            >
-                              <div class="workspace-icon icon">
-                                <span class="letter">M</span>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="workspace-item-name-wrapper">
-                            <div
-                              class="ds-editable-component workspace-item-name"
-                              style="width: auto; height: auto"
-                            >
-                              <div class="ds-text-component" dir="auto">
-                                <!-- <span>Main workspace</span> -->
-                                <template v-for="team in $page.user.all_teams">
-                                  <form
-                                    @submit.prevent="switchToTeam(team)"
-                                    :key="team.id"
-                                  >
-                                    <span
-                                      v-if="
-                                        team.id == $page.user.current_team_id
-                                      "
-                                    >
-                                      <jet-responsive-nav-link
-                                        :href="
-                                          route(
-                                            'teams.show',
-                                            $page.user.current_team
-                                          )
-                                        "
-                                        :active="route().current('teams.show')"
-                                      >
-                                        {{ team.name }}
-                                      </jet-responsive-nav-link>
-                                    </span>
-                                  </form>
-                                </template>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="application-content-wrapper first-level-control-pinned">
-          <div
-            class="first-level-content"
-            id="first-level-content"
-            style="margin-right: 0px"
-          >
-            <slot></slot>
-          </div>
-          <div class="livechat-button">
-            <span class="far fa-comments fa-2x text-center"> </span>
-          </div>
-        </div>
+      <div id="workspace">
+        <slot></slot>
       </div>
       <!-- Profile Dialog -->
       <div
@@ -255,8 +178,8 @@
                   </div>
                 </a>
                 <div class="ds-menu-item">
-                  <div class="ds-icon"><i class="fal fa-clock"></i></div>
-                  <span class="ds-title">Active Session</span>
+                  <div class="ds-icon"><i class="fal fa-users"></i></div>
+                  <span class="ds-title">teams</span>
                 </div>
                 <div class="ds-menu-item">
                   <div class="ds-icon"><i class="fal fa-lock-alt"></i></div>
